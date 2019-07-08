3e667e090d6b71fc6b4439a1eb2283d03e49da5b
Fanbo Meng <46536567+fanbomeng97@users.noreply.github.com>
2019-07-08 11:10:56 +0100

    LENS: * Add SceneManger script
    Added the script for scene chage

---
+++ HoloRepositoryDemoApplication/Assets/Scripts/ScenesManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    public void ToStartScene(string sceneName)
+    {
+        SceneManager.UnloadSceneAsync(2);
+        SceneManager.LoadScene(1, LoadSceneMode.Additive);
+    }
+
+    public void ToDetailScene(string sceneName)
+    {
+        SceneManager.UnloadSceneAsync(1);
+        SceneManager.LoadScene(2, LoadSceneMode.Additive);
+    }
+
+    public void StartApp(string sceneName)
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Additive);
+    }
+
+    public void RemoveComponent()
+    {
+        Destroy(gameObject);
+    }
+}
