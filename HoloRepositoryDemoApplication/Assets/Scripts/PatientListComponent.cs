89caac90d55d8d5e755f8b64d9b3805732b92861
Fanbo Meng <46536567+fanbomeng97@users.noreply.github.com>
2019-07-10 14:11:55 +0100

    LENS: * Add PatientListComponent.cs script
    This script used to be added in the patient list component, define some function to change the context of the component

---
+++ HoloRepositoryDemoApplication/Assets/Scripts/PatientListComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PatientListComponent : MonoBehaviour
+{
+    [SerializeField]
+    private Text listText;
+
+    public void SetText(string mytext)
+    {
+        listText.text = mytext;
+    }
+}
