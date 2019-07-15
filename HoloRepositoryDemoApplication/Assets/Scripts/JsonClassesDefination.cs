de649425bca80bca36b8bd5a3d06a0672a352eec
fanbomeng97 <46536567+fanbomeng97@users.noreply.github.com>
2019-07-15 10:30:04 +0100

    LENS: * Add JsonClassesDefination.cs
    When mapping the Json data into Unity,  it requires to create arelated C# class

---
+++ HoloRepositoryDemoApplication/Assets/Scripts/JsonClassesDefination.cs
@@ -0,0 +1,56 @@
+﻿public class PatientInfo
+{
+    public string id;
+    public PersonName name;
+    public string gender;
+    public string email;
+    public string phone;
+    public string dateOfBirth;
+    public string pictureUrl;
+    public PersonAddress address;
+    public string[] imagingStudySeries;
+    public HoloGrams[] holograms;
+}
+
+public class HoloGrams
+{
+    public string gender;
+    public string email;
+    public string phone;
+    public Person subject;
+    public Person author;
+    public string createDate;
+    public int fileSizeInkb;
+    public string imagingStudySeriesId;
+}
+
+public class PersonName
+{
+    public string title;
+    public string full;
+    public string first;
+    public string last;
+}
+
+public class PersonAddress
+{
+    public string street;
+    public string city;
+    public string state;
+    public int postcode;
+}
+
+public class Person
+{
+    public string id;
+    public PersonName name;
+}
+
+public class employee
+{
+    public string id;
+    public string employee_name;
+    public string emloyee_salary;
+    public string employee_age;
+    public string profile_image;
+}
