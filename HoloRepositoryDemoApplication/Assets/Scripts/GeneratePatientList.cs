1b1f58e9c7063d47466ec30364127003751f3a63
fanbomeng97 <46536567+fanbomeng97@users.noreply.github.com>
2019-07-15 10:25:44 +0100

    LENS: * Update the GenerationPatientList script
    Now the script can extract the patient info from both json file and website

--- HoloRepositoryDemoApplication/Assets/Scripts/GeneratePatientList.cs
+++ HoloRepositoryDemoApplication/Assets/Scripts/GeneratePatientList.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using LitJson;
+using UnityEngine.Networking;
 
 public class GeneratePatientList : MonoBehaviour
 {
@@ -10,17 +11,47 @@ public class GeneratePatientList : MonoBehaviour
     private GameObject buttonTemplates;
 
     private List<PatientInfo> patientList = new List<PatientInfo>();
+    private List<employee> employeeList = new List<employee>();
 
-    [System.Serializable]
-    public class PatientInfo
+
+
+    IEnumerator GetRequest(string uri)
     {
-        public string name;
-        public string dateOfBirth;
-        public string gender;
-
-        public static PatientInfo CreateFromJSON(string jsonString)
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
         {
-            return JsonUtility.FromJson<PatientInfo>(jsonString);
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log("Web Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log(pages[page] + ":Received: " + webRequest.downloadHandler.text);
+            }
+            string json = webRequest.downloadHandler.text;
+
+            JsonData jsonData = JsonMapper.ToObject(json);
+            for (int i = 0; i < jsonData.Count; i++)
+            {
+                employee employe = JsonMapper.ToObject<employee>(jsonData[i].ToJson());
+                employeeList.Add(employe);
+            }
+            foreach (employee patient in employeeList)
+            {
+                Debug.Log(patient.id);
+                GameObject button = Instantiate(buttonTemplates) as GameObject;
+                button.SetActive(true);
+
+                button.GetComponent<PatientListComponent>().SetID(patient.id);
+                button.GetComponent<PatientListComponent>().SetText("Patient name: " + patient.employee_name + "\nAge: " + patient.employee_age);
+
+                button.transform.SetParent(buttonTemplates.transform.parent, false);
+            }
         }
     }
 
@@ -35,19 +66,21 @@ public class GeneratePatientList : MonoBehaviour
             PatientInfo patient = JsonMapper.ToObject<PatientInfo>(jsonData[i].ToJson());
             patientList.Add(patient);
         }
-    }
-
-    void Start()
-    {
-        ReadJsonFile();
         foreach (PatientInfo patient in patientList)
         {
             GameObject button = Instantiate(buttonTemplates) as GameObject;
             button.SetActive(true);
 
-            button.GetComponent<PatientListComponent>().SetText("Patient name: " + patient.name + "\nGender: " + patient.gender);
+            button.GetComponent<PatientListComponent>().SetID(patient.id);
+            button.GetComponent<PatientListComponent>().SetText("Patient name: " + patient.name.first + " " + patient.name.last + "\nGender: " + patient.gender + "\nDate of birth: " + patient.dateOfBirth.Substring(0, 10));
 
             button.transform.SetParent(buttonTemplates.transform.parent, false);
         }
+    }
+
+    void Start()
+    {
+        ReadJsonFile();
+        //StartCoroutine(GetRequest("http://dummy.restapiexample.com/api/v1/employees"));   
     }
 }
