709615b80aea2eb3868e5d8e5214a128f80df74a
fanbomeng97 <46536567+fanbomeng97@users.noreply.github.com>
2019-07-15 14:56:07 +0100

    LENS: * Add LoginAuthentication script

---
+++ HoloRepositoryDemoApplication/Assets/Scripts/LoginAuthentication.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class LoginAuth : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_InputField PIN;
+    [SerializeField]
+    private TextMeshProUGUI WarningInfo;
+    [SerializeField]
+    private GameObject PINPad;
+
+    private void Start()
+    {
+        TMP_InputField PIN = GetComponent<TMP_InputField>();
+    }
+
+    public void Auth()
+    {
+        if (PIN.text == "3825")
+        {
+            SceneManager.LoadScene(1, LoadSceneMode.Additive);
+            ScenesManager.RemoveComponent(PINPad);
+        }
+        else
+        {
+            WarningInfo.text = "The PIN is wrong!";
+            PIN.Select();
+            PIN.text = "";
+        }
+    }
+
+    public void Clear()
+    {
+        WarningInfo.text = "";
+        PIN.Select();
+        PIN.text = "";
+    }
+}
