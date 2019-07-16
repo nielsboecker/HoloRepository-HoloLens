e217c999c2aa0926fd6f23d6c705f1dd3ecbb523
fanbomeng97 <46536567+fanbomeng97@users.noreply.github.com>
2019-07-16 15:16:13 +0100

    LENS: * Add HologramListComponent.cs
    This cript is used to change the content of Hologram list component

--- HoloRepositoryDemoApplication/Assets/Scripts/HologramListComponent.cs
+++ HoloRepositoryDemoApplication/Assets/Scripts/HologramListComponent.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class HologramList : MonoBehaviour
+public class HoloGramsListComponent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private TextMeshProUGUI HologramInfo;
+    [SerializeField]
+    private Text HologramID;
+
+    private void Start()
     {
-        
+        HologramInfo = GetComponent<TextMeshProUGUI>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetText(string HologramInformation)
     {
-        
+        HologramInfo.text = HologramInformation;
+    }
+    public void SetID(string id)
+    {
+        HologramID.text = id;
     }
 }
