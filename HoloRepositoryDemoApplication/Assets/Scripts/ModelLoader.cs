bfd49305d4473d0be970cdd50c1a734bd497028d
fanbomeng97 <46536567+fanbomeng97@users.noreply.github.com>
2019-07-16 15:45:21 +0100

    LENS: * Small chang to the ModelLoader.cs

--- HoloRepositoryDemoApplication/Assets/Scripts/ModelLoader.cs
+++ HoloRepositoryDemoApplication/Assets/Scripts/ModelLoader.cs
@@ -13,11 +13,12 @@ namespace Microsoft.MixedReality.Toolkit.Examples.Demos.Gltf
         [SerializeField]
         [Tooltip("This can be a local or external resource uri.")]
         private string uri = "https://holoblob.blob.core.windows.net/test/DamagedHelmet-18486331-5441-4271-8169-fcac6b7d8c29.glb";
-
+        [SerializeField, TooltipAttribute("Enter the build index of scene that you want to load the 3D model")]
+        private int SceneIndex = 0;
 
         public async void Start()
         {
-            Scene ModelDisplayScene = SceneManager.GetSceneByBuildIndex(2);
+            Scene ModelDisplayScene = SceneManager.GetSceneByBuildIndex(SceneIndex);
             Response response = new Response();
 
             try
