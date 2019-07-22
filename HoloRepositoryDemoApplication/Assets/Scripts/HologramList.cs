8324b70e8602ee3fffe37bf0e1fbfa49f5b29af7
fanbomeng97 <46536567+fanbomeng97@users.noreply.github.com>
2019-07-22 13:00:32 +0100

    LENS: Update HologramListScene

--- HoloRepositoryDemoApplication/Assets/Scripts/HologramList.cs
+++ HoloRepositoryDemoApplication/Assets/Scripts/HologramList.cs
@@ -24,18 +24,20 @@ public class HologramList : MonoBehaviour
             {
                 Message.text = "There is no Holograms for this patient";
             }
-
-            foreach (Hologram hologram in PatientListItem.HologramsList)
+            else
             {
-                GameObject button = Instantiate(buttonTemplates) as GameObject;
-                button.SetActive(true);
+                foreach (Hologram hologram in PatientListItem.HologramsList)
+                {
+                    GameObject button = Instantiate(buttonTemplates) as GameObject;
+                    button.SetActive(true);
 
-                button.GetComponent<HologramListItem>().SetID(hologram.hid);
-                string Info = string.Format("Hologram name: {0}\nTitle: {1}\nDate of Creation: {2}", hologram.subject.name.full, hologram.title, hologram.createdDate.Substring(0, 10));
-                button.GetComponent<HologramListItem>().SetText(Info);
+                    button.GetComponent<HologramListItem>().SetID(hologram.hid);
+                    string Info = string.Format("Hologram name: {0}\nTitle: {1}\nDate of Creation: {2}", hologram.subject.name.full, hologram.title, hologram.createdDate.Substring(0, 10));
+                    button.GetComponent<HologramListItem>().SetText(Info);
 
-                button.transform.SetParent(buttonTemplates.transform.parent, false);
-            }
+                    button.transform.SetParent(buttonTemplates.transform.parent, false);
+                }
+            }            
         }        
     }
 }
