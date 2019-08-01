af0dbc3617d9b2e01941aaf637c08df1ccb67d83
Fanbo Meng <46536567+fanbomeng97@users.noreply.github.com>
2019-08-01 17:50:25 +0100

    LENS: Use connector package for HoloApp

--- HoloRepositoryDemoApplication/Assets/Scripts/HologramListItem.cs
+++ HoloRepositoryDemoApplication/Assets/Scripts/HologramListItem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using HoloStorageConnector;
 
 public class HologramListItem : MonoBehaviour
 {
@@ -23,5 +24,15 @@ public class HologramListItem : MonoBehaviour
     public void SetID(string id)
     {
         HologramID.text = id;
+    }
+
+    public void LoadHologram()
+    {
+        ModelSetting.SetSceneName("ModelDisplayScene");
+        ModelSetting.SetManipulable(true);
+        ModelSetting.SetRotation(new Vector3(0, 180, 0));
+        ModelSetting.SetPostition(new Vector3(0f, 0f, 2f));
+        ModelSetting.SetSize(0.5f);
+        HoloStorageClient.LoadHologram("hid");
     }
 }
