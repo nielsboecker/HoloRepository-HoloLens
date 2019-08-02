c2d04fd713c0e3deb8be32f4af57f50b9c4ef4ec
fanbomeng97 <46536567+fanbomeng97@users.noreply.github.com>
2019-08-02 12:59:12 +0100

    LENS: Fixed a scene error

--- HoloRepositoryDemoApplication/Assets/Scripts/HologramListItem.cs
+++ HoloRepositoryDemoApplication/Assets/Scripts/HologramListItem.cs
@@ -29,9 +29,10 @@ public class HologramListItem : MonoBehaviour
     public void LoadHologram()
     {
         HologramInstantiationSettings setting = new HologramInstantiationSettings();
-        setting.Name = "ModelDisplayScene";
+        setting.Name = "Loaded Model";
         setting.Rotation = new Vector3(0, 180, 0);
         setting.Position = new Vector3(0f, 0f, 2f);
+        setting.SceneName = "ModelDisplayScene";
         setting.Size = 0.5f;
         HoloStorageClient.LoadHologram(setting, "hid");
     }
