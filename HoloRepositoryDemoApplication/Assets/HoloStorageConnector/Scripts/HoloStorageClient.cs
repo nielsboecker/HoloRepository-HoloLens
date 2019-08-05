e8e25d1e07bf6a5d05840e9adef8f4acfd4ccfc8
fanbomeng97 <46536567+fanbomeng97@users.noreply.github.com>
2019-08-05 16:39:18 +0100

    LENS: Clean up code

--- HoloRepositoryDemoApplication/Assets/HoloStorageConnector/Scripts/HoloStorageClient.cs
+++ HoloRepositoryDemoApplication/Assets/HoloStorageConnector/Scripts/HoloStorageClient.cs
@@ -66,7 +66,7 @@ namespace HoloStorageConnector
                         patientList.Add(patient);
                     }
                 }
-            }            
+            }
         }
 
         /// <summary>
