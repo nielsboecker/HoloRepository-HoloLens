3c2fc4d62526b8cc6c5ea429892c502dfeff20ec
fanbomeng97 <46536567+fanbomeng97@users.noreply.github.com>
2019-08-05 10:30:42 +0100

    LENS: Change entities to API 1.0 specification

--- HoloRepositoryDemoApplication/Assets/HoloStorageConnector/Scripts/HoloStorageEntities.cs
+++ HoloRepositoryDemoApplication/Assets/HoloStorageConnector/Scripts/HoloStorageEntities.cs
@@ -8,48 +8,33 @@ namespace HoloStorageConnector
     public class Patient
     {
         public string pid { get; set; }
-        public PersonName name { get; set; }
         public string gender { get; set; }
-        public string email { get; set; }
-        public string phone { get; set; }
         public string birthDate { get; set; }
-        public string pictureUrl { get; set; }
-        public Address address { get; set; }
-        public string[] imagingStudySeries { get; set; }
-        public Hologram[] holograms { get; set; }
+        public PersonName name { get; set; }
     }
 
     public class Hologram
     {
         public string hid { get; set; }
         public string title { get; set; }
-        public Subject subject { get; set; }
-        public Author author { get; set; }
-        public string createdDate { get; set; }
+        public string description { get; set; }
+        public string contentType { get; set; }
         public int fileSizeInkb { get; set; }
-        public string imagingStudySeriesId { get; set; }
+        public string bodySite { get; set; }
+        public string dateOfImaging { get; set; }
+        public string creationDate { get; set; }
+        public string creationMode { get; set; }
+        public string creationDescription { get; set; }
+        public string aid { get; set; }
+        public string pid { get; set; }
     }
 
     public class PersonName
     {
         public string title { get; set; }
         public string full { get; set; }
-        public string first { get; set; }
-        public string last { get; set; }
-    }
-
-    public class Address
-    {
-        public string street { get; set; }
-        public string city { get; set; }
-        public string state { get; set; }
-        public int postcode { get; set; }
-    }
-
-    public class Subject
-    {
-        public string pid { get; set; }
-        public PersonName name { get; set; }
+        public string given { get; set; }
+        public string family { get; set; }
     }
 
     public class Author
