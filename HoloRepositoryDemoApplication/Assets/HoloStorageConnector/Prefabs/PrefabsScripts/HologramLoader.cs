73bd4ec049b0dd7863ed9bf8642b945a587112ec
fanbomeng97 <46536567+fanbomeng97@users.noreply.github.com>
2019-08-09 12:11:58 +0100

    LENS; Added prefabs scripts

---
+++ HoloRepositoryDemoApplication/Assets/HoloStorageConnector/Prefabs/PrefabsScripts/HologramLoader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using HoloStorageConnector;
+
+public class HologramLoader : MonoBehaviour
+{
+    public string HologramID = string.Empty;
+    public string SceneName;
+    public string ModelName = "LoadedModel";
+    public Vector3 ModelPosition = new Vector3(0f, 0f, 0f);
+    public Vector3 ModelRotation = new Vector3(0f, 0f, 0f);
+    public float ModelSize = 1.0f;
+    public bool Manipulable = true;
+
+    void Start()
+    {
+        HologramInstantiationSettings setting = new HologramInstantiationSettings
+        {
+            Name = ModelName,
+            Position = ModelPosition,
+            Rotation = ModelRotation,
+            Manipulable = true,
+            Size = ModelSize
+        };
+
+        if (SceneName != string.Empty)
+        {
+            setting.SceneName = SceneName;
+        }
+        
+        HoloStorageClient.LoadHologram(HologramID, setting);
+    }
+}
